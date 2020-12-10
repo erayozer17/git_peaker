dba76c30bad35dde7f5d938b0d58b5eb67a3bdf2
erayozer17 <erayozer91@protonmail.com>
2020-12-10 13:16:53 +0100

    add api tests

--- src/__init__.py
+++ src/__init__.py
@@ -26,9 +26,8 @@ def is_user_active(user):
     contribution_days = make_user_contribution_call(user)
     if not contribution_days and type(contribution_days) != int:
         return jsonify({
-            "status": 400,
             "message": f"{user} not found."
-        })
+        }), 400
     return jsonify({
         "is_active_since_yesterday": contribution_days > 0
     })
@@ -39,9 +38,8 @@ def is_repo_downgraded(repo):
     total_changes_last_week = get_total_changes_on_commits(repo)
     if not total_changes_last_week and type(total_changes_last_week) != int:
         return jsonify({
-            "status": 400,
             "message": f"{repo} is not unique or not found."
-        })
+        }), 400
     return jsonify({
         "is_repo_downwarded_since_last_week": True if total_changes_last_week < 0 else False
     })
--- src/tests/test_api.py
+++ src/tests/test_api.py
@@ -6,13 +6,47 @@ from src.tests.base import BaseTestCase
 class TestUserService(BaseTestCase):
     """Tests for the Users Service."""
 
-    def test_users(self):
+    def test_sanity(self):
         """Ensure the /sanity route behaves correctly."""
         response = self.client.get('/sanity')
         data = json.loads(response.data.decode())
         self.assertEqual(response.status_code, 200)
         self.assertIn('check!', data['message'])
         self.assertIn('success', data['status'])
+
+    def test_active(self):
+        """Ensure the /active/<user> route behaves correctly."""
+        response = self.client.get('/active/erayozer17')
+        data = json.loads(response.data.decode())
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('is_active_since_yesterday', data)
+        assert isinstance(data['is_active_since_yesterday'], bool)
+
+    def test_downwards(self):
+        """Ensure the /downwards/<repo> route behaves correctly."""
+        response = self.client.get('/downwards/git_peaker')
+        data = json.loads(response.data.decode())
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('is_repo_downwarded_since_last_week', data)
+        assert isinstance(data['is_repo_downwarded_since_last_week'], bool)
+
+    def test_active_not_found(self):
+        """Ensure the /active/<user> route behaves correctly when not found."""
+        user = "hubduhvhdbfuvdfbvudfbvufvyb"
+        response = self.client.get(f'/active/{user}')
+        data = json.loads(response.data.decode())
+        self.assertIn('message', data)
+        self.assertEqual(response.status_code, 400)
+        assert f"{user} not found." == data['message']
+
+    def test_downwards_not_found(self):
+        """Ensure the /downwards/<repo> route behaves correctly when not found."""
+        repo = "hubduhvhdbfuvdfbvudfbvufvyb"
+        response = self.client.get(f'/downwards/{repo}')
+        data = json.loads(response.data.decode())
+        self.assertIn('message', data)
+        self.assertEqual(response.status_code, 400)
+        assert f"{repo} is not unique or not found." == data['message']
 
 
 if __name__ == '__main__':
